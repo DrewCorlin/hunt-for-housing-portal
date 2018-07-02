75af6dbbaa4d2c2eff6bcd6bb307cd6c484c58b1
Drew Corlin <dcorlin@yahoo.com>
2018-07-01 17:16:03 -0800

    moved underscore from dev to dependency

--- app/GlobalEvents.js
+++ app/GlobalEvents.js
@@ -1,10 +1,12 @@
 // Non view-specific events avaliable to global app channel
 export default {
+    // Expects return from requester
     requests: {
         "user:authToken": function() {
             return window.serverSession.authToken;
         }
     },
+    // No return expected
     triggers: {
         'set:user:authToken': function(authToken) {
             window.serverSession.authToken = authToken;
